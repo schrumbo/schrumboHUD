a7e56339d0b9eaef0ba37e43365f2257d9c69bf7
Max Aydt <aydt.maximilian@gmail.com>
2026-01-12 14:17:12 +0100

    add modmenu integration

---
+++ versions/1.21.11/src/client/java/schrumbo/schrumbohud/modmenu/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package schrumbo.schrumbohud.modmenu;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import schrumbo.schrumbohud.clickgui.ClickGuiScreen;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> new ClickGuiScreen();
+    }
+}
