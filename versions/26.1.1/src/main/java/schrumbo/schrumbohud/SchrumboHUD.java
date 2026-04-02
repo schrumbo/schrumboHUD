b5bce24a1c0f758bc7152ce831e70e1753fe914d
schrumbo <aydt.maximilian@gmail.com>
2026-04-02 04:00:56 +0200

    add 26.1.1 support

---
+++ versions/26.1.1/src/main/java/schrumbo/schrumbohud/SchrumboHUD.java
@@ -0,0 +1,19 @@
+package schrumbo.schrumbohud;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Common mod entrypoint
+ */
+public class SchrumboHUD implements ModInitializer {
+	public static final String MOD_ID = "schrumbohud";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
